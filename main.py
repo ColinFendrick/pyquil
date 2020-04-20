967aedeeabf587652fb0fee7019f8d1650f9427e
Colin Fendrick <colin.fendrick@gmail.com>
2020-04-20 09:27:30 -0400

    :sparkles: Basic wavefunc and xor

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from pyquil import get_qc, Program
-from pyquil.gates import *
-from pyquil.api import local_forest_runtime
+from pyquil.gates import Z, CNOT, H
+from pyquil.api import local_forest_runtime, WavefunctionSimulator
+from pyquil.paulis import sZ
+
+z0 = (1-sZ(0))*0.5
+z1 = (1-sZ(1))*0.5
+xor = (1-sZ(0)*sZ(1))*0.5
+print (xor)
 
 prog = Program(Z(0), CNOT(0, 1))
 
@@ -16,3 +22,6 @@ qc = get_qc('9q-square-qvm')
 result = qc.run_and_measure(p, trials=10)
 print(result[0])
 print(result[1])
+
+wfn = WavefunctionSimulator().wavefunction(p)
+print(wfn)

1ce6955b25604d8d4eb7dc37c6254777542042eb
Colin Fendrick <colin.fendrick@gmail.com>
2020-04-21 13:35:58 -0400

    :sparkles: Batch job using asarray

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from pyquil import get_qc, Program
-from pyquil.gates import Z, CNOT, H
-from pyquil.api import local_forest_runtime, WavefunctionSimulator
+from pyquil.gates import Z, CNOT, H, RY
+from pyquil.api import local_forest_runtime, WavefunctionSimulator, QVMConnection
 from pyquil.paulis import sZ
+import numpy as np
+from matplotlib import pyplot as plt
 
 z0 = (1-sZ(0))*0.5
 z1 = (1-sZ(1))*0.5
@@ -26,7 +28,16 @@ print(result[1])
 wfn = WavefunctionSimulator().wavefunction(p)
 print(wfn)
 
-
 for observable in [z0, z1, xor]:
     expectation = WavefunctionSimulator().expectation(prep_prog=p, pauli_terms=observable)
     print(observable, '\t', expectation)
+
+cxn = QVMConnection()
+thetas = np.linspace(0, np.pi, 20)
+bitstrings = [np.asarray(cxn.run_and_measure(Program(RY(theta, 0)), qubits=[0], trials=1000))
+              for theta in thetas]
+
+
+averages = [np.mean(bs[:,0]) for bs in bitstrings]
+plt.plot(thetas, averages, 'o-')
+plt.show()

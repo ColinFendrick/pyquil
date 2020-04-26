c7785df6d2d85369d58de15d0a9b666cd861ba55
Colin Fendrick <colin.fendrick@gmail.com>
2020-04-26 10:27:49 -0400

    :pencil: refactor topology

--- topology.py
+++ topology.py
@@ -2,11 +2,6 @@ from pyquil import get_qc
 import networkx as nx
 from matplotlib import pyplot as plt
 
-qc = get_qc('9q-square-qvm')
-nx.draw(qc.qubit_topology())
-plt.title('9q-square-qvm', fontsize=18)
-plt.show()
-
 """
 WavefunctionSimulator still has no notion of qubit connectivity,
 so feel free to use that for simulating quantum algorithms that you aren’t concerned about running on an actual QPU.
@@ -17,14 +12,12 @@ These QVM’s are constructed with a topology!
 It just happens to be fully connected.
 """
 
-nx.draw(get_qc('5q-qvm').qubit_topology())
-plt.title('5q-qvm is fully connected', fontsize=16)
-plt.show()
+def nqqvm(n):
+    nx.draw(get_qc('{}q-qvm'.format(n)).qubit_topology())
+    plt.title('{}q-qvm is fully connected'.format(n), fontsize=16)
+    plt.show()
 
-nx.draw(get_qc('22q-qvm').qubit_topology())
-plt.title('5q-qvm is fully connected', fontsize=16)
-plt.show()
-
-nx.draw(get_qc('222q-qvm').qubit_topology())
-plt.title('5q-qvm is fully connected', fontsize=16)
-plt.show()
+nqqvm(5)
+nqqvm(9)
+nqqvm(22)
+nqqvm(234)
